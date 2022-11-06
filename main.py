3473a9ed27cd2f182f80300647aaeafe2660fb15
Heath Hilton <hhilton2@ivytech.edu>
2022-11-06 17:13:34 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Heath Hilton
+# 11/06/2022
+
+# Program: 6.1 - Print the List
+
+# The program is to print out the items in a list
+
+
+forLoopList = [3, 2, 1, 0]
+
+for x in forLoopList:
+  print(x)
